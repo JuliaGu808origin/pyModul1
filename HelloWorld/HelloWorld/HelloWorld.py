14124b9282f8d04307109045afa089e44a03efd9
Julia Gu <juliagu808@gmail.com>
2020-09-09 15:43:44 +0200

    a bit

--- HelloWorld/HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld/HelloWorld.py
@@ -1,2 +1,2 @@
 name = "Hello World"
-print(f"name is {name}")
+print(name)
---
+++ HelloWorld/HelloWorld/var.2.py
@@ -0,0 +1,13 @@
+myName= "Julia Gu"
+print(f"Hello {myName}")
+
+age = 25
+print(f"{myName} is {age} years old")
+
+firstname = input("ditt förnamn: ")
+lastname = input("ditt efternamn: ")
+print(f"Du heter: {lastname}, {firstname}")
+
+tal1 = int(input("tal 1 : "))
+tal2 = int(input("tal 2 : "))
+print(f"total is : {tal1+tal2}")
